7fcce465b76f04bd19177aac0356209e054b7cfc
hijaswinder44488 <jaswinderkaur44888@gmail.com>
2024-05-28 16:33:24 +0530

    file

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name="loan_cla",
     version='0.0.1',
-    author='vishal',
-    author_email='vishalsinghrawat505@gmail.com',
+    author='jaswinder kaur',
+    author_email='jaswinderkaur44888@gmail.com',
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
     )
